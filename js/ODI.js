7b443520e0cf3fc1aeb5b11960ebe2b076724cf0
Luis-707 <lgalanton4@gmail.com>
2024-08-28 09:38:19 -0400

    agregacion_odi1

--- js/ODI.js
+++ js/ODI.js
@@ -40,13 +40,26 @@ function listarODI(){
 function listarODI(respuestaServidor){
   console.log(respuestaServidor);
   var data = respuestaServidor[0];
+
+  if ($.fn.dataTable.isDataTable('#t_odi')) {
+    // Si la tabla ya está inicializada, actualiza sus datos
+    let table = $('#t_odi').DataTable();
+    table.clear();
+    table.rows.add(datos);
+    table.draw();
+} else {
+    // Si la tabla no está inicializada, inicialízala
+    // los nombres de data tien que coincidir con los de los campos de la consulta no 
+    // para aqgreagar otro campo  { data: 'nombre_Del_campo' } de la la base de datos.
   $('#t_odi').DataTable( {
-    data: data,
+    data: datos,
     columns: [
         { data: 'odi_id',title: 'CODIGO'  },
         { data: 'nombre_odi',title: 'NOMBRE DE ODI' },
         { data: 'peso',title: 'PESO' },
         { data: 'status',title: 'STATUS' }
       ]
-  } );
+  });
+
+}
 }
